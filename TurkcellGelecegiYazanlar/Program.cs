34671eb38eca36122b3ec0227b84f564d451c3fc
Emre Kaan Seylim <seylimemre@gmail.com>
2022-01-18 23:04:37 +0300

    IndexOf, Contains ve Split fonksiyonlarına örnek.

--- TurkcellGelecegiYazanlar/Program.cs
+++ TurkcellGelecegiYazanlar/Program.cs
@@ -6,7 +6,49 @@ namespace TurkcellGelecegiYazanlar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //indexOf()
+            Console.Write("Bir Kelime Giriniz: ");
+            string kelimeIndexOf = Console.ReadLine();
+
+            Console.Write("Bir Harf Giriniz: ");
+            string harfIndexOf = Console.ReadLine();
+
+            if (kelimeIndexOf.IndexOf(harfIndexOf) < 0)
+            {
+                Console.WriteLine("Aradığınız Harf '{0}' Girdiğiniz Kelimede Bulunamadı.", harfIndexOf);
+            }
+            else
+            {
+                Console.WriteLine("{0} Harf {1}. Indexte", harfIndexOf, kelimeIndexOf.IndexOf(harfIndexOf));
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+
+            //Contains()
+            Console.Write("Bir Cümle Girniz: ");
+            string cumle = Console.ReadLine();
+
+            Console.Write("Bir Kelime Giriniz: ");
+            string kelimeConstains = Console.ReadLine();
+
+            if (cumle.Contains(kelimeConstains))
+            {
+                Console.WriteLine("Cümlenin İçinde '{0}' Kelimesi Geçiyor.", kelimeConstains);
+            }
+            else
+            {
+                Console.WriteLine("Cümlenin İçinde '{0}' Kelimesi Geçmiyor.", kelimeConstains);
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+
+
+            //split()
+            char[] ayrac = { '/', ' ', ':' };
+            Console.Write("GÜN AY YIL Şekilinde Bir Tarih Giriniz: ");
+            string tarih = Console.ReadLine();
+            string[] tarihDizisi = tarih.Split(ayrac);
+            Console.WriteLine("GÜN: {0}, AY: {1}, YIL: {2}", tarihDizisi[0], tarihDizisi[1], tarihDizisi[2]);
         }
     }
 }
